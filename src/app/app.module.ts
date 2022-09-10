b5273d85116cc0cce2928aecd114dff5373337ef
Ayush Pawar <apawar@affinionds.com>
2022-09-11 01:06:38 +0530

    setup routes and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ActivityModule } from './modules/activity/activity.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeModule } from './modules/home/home.module';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ACTIVITY_API_BASE_URL :'https://activity-loyalty-service.qa.cnxloyalty.com/activity/v1.0/search',
+  CARS_API_BASE_URL:'https://car-loyalty.qa.cnxloyalty.com//car/v1.0/search',
+  HOTELS_API_BASE_URL:'https://hotel-loyalty.stage.cnxloyalty.com/hotel/v1.0/search',
+  AUTOSUGGEST_API_BASE_URL:'https://orxe.qa.cnxloyalty.com/api/autosuggest/v1.0/search',
+  AUTOSUGGEST_API_AUTH_TOKEN:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbngtdGVuYW50SWQiOiIzZTFnbzRpbHhxOCIsImNueC1zZXNzaW9uaWQiOiI1OTY1MjJjZC0xMjE4LTQ5YWQtOGI4YS1iMGY0ZDgxOTBiOTUiLCJjbngtY29ycmVsYXRpb25JZCI6IjM3NTliMTczLTJjNDEtZWExMC02MWZiLWRhZDZkNmU5Y2JjOSIsIkNTUkYtVG9rZW4iOiJhODE2Y2NiNy04NzRhLTQxM2UtYTM4Yy1mZDIxM2Q5NWNhODkiLCJjbGllbnQtaWQiOiIxMDMiLCJwcm9ncmFtLWlkIjoiMTM4OSIsInByb2dyYW0tZ3JvdXAtaWQiOiI1MDgiLCJkZXZpY2UtdHlwZSI6Ik1vYmlsZSIsInBhcnRpY2lwYW50LWNvZGUiOiI2NjU5OTY2MyIsIm5iZiI6MTY2MjgzNTgyNywiZXhwIjoxNjYyODM5NDI3LCJpYXQiOjE2NjI4MzU4MjcsImlzcyI6Imh0dHBzOi8vb3J4ZS1hcGkucWEuY254bG95YWx0eS5jb20ifQ.rn6cDEePGOipKlHleTz9GVMMP9-NshxHajaCvmmrFow',
+  AUTOSUGGEST_API_SESSION_ID:'596522cd-1218-49ad-8b8a-b0f4d8190b95',
 };
 
 /*
