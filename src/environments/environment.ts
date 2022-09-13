fa80939cb96ed21b92d2497bfba4aba483590868
Ayush Pawar <apawar@affinionds.com>
2022-09-13 12:50:36 +0530

    updated env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   CARS_API_BASE_URL:'https://car-loyalty.qa.cnxloyalty.com/car/v1.0/search',
   HOTELS_API_BASE_URL:'https://hotel-loyalty.stage.cnxloyalty.com/hotel/v1.0/search',
   AUTOSUGGEST_API_BASE_URL:'https://orxe.qa.cnxloyalty.com/api/autosuggest/v1.0/search',
-  AUTOSUGGEST_API_AUTH_TOKEN:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbngtdGVuYW50SWQiOiIzZTFnbzRpbHhxOCIsImNueC1zZXNzaW9uaWQiOiJiNTg5MzE2ZC1mMzU4LTQ0Y2MtYjVkMC1kMzM4YTE5YzllMzUiLCJjbngtY29ycmVsYXRpb25JZCI6IjY3OTFmYzRjLWFhYmQtMGZiNi1lYmQ0LTQzMTA2NzMyZTU2YiIsIkNTUkYtVG9rZW4iOiI4N2QxNGM4MS1iZDkxLTRiYWItOTQ4Mi1kNjU1MjYzZjFmOGUiLCJjbGllbnQtaWQiOiIxMDMiLCJwcm9ncmFtLWlkIjoiMTM4OSIsInByb2dyYW0tZ3JvdXAtaWQiOiI1MDgiLCJkZXZpY2UtdHlwZSI6Ik1vYmlsZSIsInBhcnRpY2lwYW50LWNvZGUiOiI4MjU4MzY2OSIsIm5iZiI6MTY2MzA1MjEzNCwiZXhwIjoxNjYzMDU1NzM0LCJpYXQiOjE2NjMwNTIxMzQsImlzcyI6Imh0dHBzOi8vb3J4ZS1hcGkucWEuY254bG95YWx0eS5jb20ifQ.-pXadDgUJQ8aCIc2VRY0M8d4HE0vFA6OO8TI9RwJyo0',
-  AUTOSUGGEST_API_SESSION_ID:'b589316d-f358-44cc-b5d0-d338a19c9e35',
+  AUTOSUGGEST_API_AUTH_TOKEN:'update token here',
+  AUTOSUGGEST_API_SESSION_ID:'enter session id',
 };
 
 /*
